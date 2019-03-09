cb49cfe37ae453df7a2c67383aacd33d3218b610
c-buchanan <41310320+c-buchanan@users.noreply.github.com>
2019-03-09 16:59:04 -0500

    organizing files
    I organized the data into raw and cleaned files, added a folder to put screenshots of visualizations, and added a folder for my k-means clustering files.

--- kmeans_analysis/k-means_analysis.R
+++ kmeans_analysis/k-means_analysis.R
@@ -16,15 +16,20 @@ library(stats)
 
 # Set the working directory
 getwd()
-setwd("C:/Users/Courtney/Documents/wine_datasets/input/")
+path.expand("~/kmeans_analysis")
+setwd()
+
 
 # Read the stats
-wines <- read.csv("combined_wine_binary.csv", header = TRUE)
+wines <- read.csv("input/combined_wine_binary.csv", header = TRUE)
 
 # View header
-head(wines) 
+head(wines, n=10) 
 
-# DATA ANALYSIS: VISUALIZATION AND DATA EXPLORATION
+# Rename the columns
+names(wines) <- c("wine_type", "fixed_acidity", "volatile_acidity", "citric acid", "residual_sugar", "chlorides", "free_sulfur_dioxide", "total_sulfur_dioxide", "density", "pH", "sulphates", "alcohol", "quality")
+
+## DATA VISUALIZATION AND DATA EXPLORATION
 
 # Determine the structure of data set
 str(wines)
@@ -43,79 +48,64 @@ wines %>%
 # Create a correlation matrix for each attribute
 corrplot(cor(wines), type="upper", method="ellipse", tl.cex=0.9)
 
-# Since there's a strong linear correlation
-# between sulfur dioxide and type, 
-# test the relationship between them 
+## K-Means Execution
 
-# Relationship between 
-# sulfur dioxide and wine type
-ggplot(wines, aes(x=total.sulfur.dioxide, y=Type)) +
-  geom_point() +
-  geom_smooth(method="lm", se=FALSE)
+# Execution of k-means with k=13
+set.seed(1002476) #Set the seed for reproducibility
 
-# K-Means Execution
+# Create 10 clusters
+wines_k13_centers <-kmeans(wines, centers=13) 
+wines_k13_centers$centers # Display cluster centers
+table(wines_k13_centers$cluster) # Give a count of data points in each cluster
 
-# Execution of k-means with k=2
-set.seed(1234)
-wines_k2 <- kmeans(wines, centers=2)
-
-# Cluster to which each point is allocated
-wines_k2$cluster
-
-# Cluster centers
-wines_k2$centers
-
-# Cluster sizes
-wines_k2$size
-
-# Between-cluster sum of squares
-wines_k2$betweenss
-
-# Within-cluster sum of squares
-wines_k2$withinss
-
-# Total within-cluster sum of squares 
-wines_k2$tot.withinss
-
-# Total sum of squares
-wines_k2$totss
+# Remove columns 1 and 13
+wines_k13 <-kmeans(wines[,-c(1,13)], centers=13) 
+wines_k13$centers # Display cluster centers
+table(wines_k13$cluster) # Give a count of data points in each cluster
+table(wines_k13_centers$cluster)
 
 # Run the algorithm for different values of k 
 bss <- numeric()
 wss <- numeric()
 
-set.seed(1234)
-for(i in 1:10){
+set.seed(1002476)
+for(i in 1:13){
   
   # For each k, calculate betweenss and tot.withinss
-  bss[i] <- kmeans(winesNorm, centers=i)$betweenss
-  wss[i] <- kmeans(winesNorm, centers=i)$tot.withinss
+  bss[i] <- kmeans(wines, centers=i)$betweenss
+  wss[i] <- kmeans(wines, centers=i)$tot.withinss
 }
 
-# Between-cluster sum of squares vs Choice of k
-p3 <- qplot(1:10, bss, geom=c("point", "line"), 
+# To study which value of k gives us the best partition
+# Between-Cluster Sum of Squares vs Choice of k
+p3 <- qplot(1:13, bss, geom=c("point", "line"), 
             xlab="Number of Clusters", ylab="Between-Cluster Sum of Squares") +
-  scale_x_continuous(breaks=seq(0, 10, 1))
+  scale_x_continuous(breaks=seq(0, 13, 1))
 
 # Total within-cluster sum of squares vs Choice of k
-p4 <- qplot(1:10, wss, geom=c("point", "line"),
+p4 <- qplot(1:13, wss, geom=c("point", "line"),
             xlab="Number of Clusters", ylab="Total Within-Cluster Sum of Squares") +
-  scale_x_continuous(breaks=seq(0, 10, 1))
+  scale_x_continuous(breaks=seq(0, 13, 1))
 
 # Subplot
 grid.arrange(p3, p4, ncol=2)
 
+# Determine the optimal k-value based on graph
+# Choose number of clusters where adding 
+# another cluster doesn’t partition data better
+# Choose k-value of 5
+
 ## RESULTS
 
-# Execution of k-means with k=3
-set.seed(1234)
-wines_k2 <- kmeans(wines, centers=3)
+# Execution of k-means with k=5
+set.seed(1002476)
+wines_k5 <- kmeans(wines, centers=5)
 
 # Mean values of each cluster
-aggregate(wines, by=list(wines_k2$cluster), mean)
+aggregate(wines, by=list(wines_k5$cluster), mean)
 
 # Clustering 
-ggpairs(cbind(wines, Cluster=as.factor(wines_k2$cluster)),
+ggpairs(cbind(wines, Cluster=as.factor(wines$quality)),
         columns=1:13, aes(colour=Cluster, alpha=0.5),
         lower=list(continuous="points"),
         upper=list(continuous="blank"),
